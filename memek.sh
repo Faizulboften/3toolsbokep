d6068d4246ebd96fcaa4d7e53bf89e9fa481f586
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-03 13:29:02 +0800

    Create memek.sh

---
+++ memek.sh
@@ -0,0 +1,97 @@
+clear
+sleep 10
+echo $cyan " 📂 MENGINSTALL 3 TOOLS BOKEP"
+sleep 20
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+clear
+sleep 1
+echo $or "
+█████████
+\033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;93m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
+\033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Dark
+\033[1;93m█████████      \033[1;92m«----------✧----------»
+\033[1;93m ██ ██"
+
+echo $or "
+╔════════════════════════════════════════════╗
+              FB : 📂 FAIZUL 
+              WA : 📂 082271426251
+              YT : 📂 HTC CTR GAMING
+              AU : 📂 https://github.com/Faizulcyber
+╚════════════════════════════════════════════╝
+
+
+════════════════════════════════════════════
+            📂 MENU 3 TOOLS BOKEP 📂
+════════════════════════════════════════════
+
+════════════════════════════════════════════
+[1] 🔘 BOKEP INDO
+════════════════════════════════════════════
+[2] 🔘 BOKEP JEPANG
+════════════════════════════════════════════
+[3] 🔘 BOKEP VIRAL INDO
+════════════════════════════════════════════
+┠
+┗────[99] exit ✘"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $cyan " 🔘 INSTALL BOKEP "
+sleep 1
+echo $cyan " 🔘 OTW COLI "
+clear
+pkg install git
+pip2 install mecahnize
+git clone https://github.com/Faizulboften/bokepindo
+cd bokepindo
+sh croot.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $cyan "📂 MENGINSTALL BOKEP "
+sleep 10
+echo $cyan "📂 HORE NONTON BOKEP"
+clear
+pkg install git
+pip2 install lolcat
+git clone https://github.com/Faizulboften/ToolsBokep
+cd ToolsBokep
+sh viral.sh
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $cyan "🔓 MENGINSTALL BOKEP "
+sleep 10
+echo $cyan " 🔓 YA ALLAH AMPUNILA DOSA MEREKA"
+sleep 10
+clear
+pkg install git
+pkg install python
+git clone https://github.com/Faizulboften/Viral
+cd Viral
+sh croot.sh
+
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "SUBREJ YT HTC CTR GAMING "
+sleep 1
+exit
+fi
