ebe1f3c282aaaa4705ab5e4f6bbefb81a33d95a5
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-23 00:17:56 +0800

    Update memek.sh

--- memek.sh
+++ memek.sh
@@ -10,9 +10,75 @@ me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
 or='\033[1;38;5;208m' #Orange
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#gantikodewarna
+k="\033[33;1m"
+m="\033[31;1m"
+pu="\033[39;1m"
+p="\033[36;1m"
+b="\033[34;1m"
+h="\033[32;1m"
+JAM=`date +%H`
+MENIT=`date +%M`
+DETIK=`date +%S`
+TANGGAL=`date +%d`
+BULAN=`date +%m`
+TAHUN=`date +%Y`
+mp="\033[41;1;39m"
+o="\033[0m"
+clear
+echo
+echo "         ${m}[ ${pu}TIME ZONE INDONESIA ${m}]"
+echo "${b}   ╔════════╗${b} ╔═══════🕜═╗${b} ╔═══════🕛═╗"
+echo "${b}   ║${k}JAM:"${m}[${pu}$JAM${m}]${b}║~${b}║${k}MENIT:"${m}[${pu}$MENIT${m}]${b}║${b}~║${k}DETIK:${m}["${pu}$DETIK${m}]${b}║
+echo "${b}   ╚════════╝${b} ╚══════════╝${b} ╚══════════╝"
+echo "               ${m}[ ${pu}KALENDER ${m}]"
+echo "${b}╔════════════╗${b} ╔══════════╗${b} ╔════════════╗"
+echo "${b}║${k}TANGGAL:"${m}[${pu}$TANGGAL${m}]${b}║${b}~║${k}BULAN:"${m}[${pu}$BULAN${m}]${b}║${b}~║${k}TAHUN:${m}["${pu}$TAHUN${m}]${b}║
+echo "${b}╚════════════╝${b} ╚══════════╝${b} ╚════════════╝"
+echo "${k}<════════════════════════════════════════>"
+echo
+
+
+limit=100
+sleep 1
+echo " \033[31;1m    ▇▇▇▇▇▇▇"
+echo "\033[37;1m     ▇▇▇▇▇▇▇"
+echo "\033[31;1mSELAMAT DATANG \033[37;1mDi Tools Faizul Kontol lu"
+sleep 1
+echo -n "My Name Is?:"
+read nama;
+sleep 3
+echo "Hallo salam kenal Sad \033[31;1m[$nama]"
+sleep 3
+echo "\033[32;1mJangan Lupa subscribe channel admin woke:)"
+sleep 3
+clear
+echo -n "apakah anda sudah subscribe channel admin? y/n :"
+read pilih
+if [ $pilih = "y" ];
+then
+sleep 2
+echo "Terimakasih sudah subscribe channel admin"
+sleep 2
+clear
+elif [ $pilih = "n" ];
+then
+sleep 2
+echo "SILAHKAN SUBSCRIBE CHANNEL ADMIN"
+termux-open-url "https://www.youtube.com/channel/UCSxkYGsMV5DvXz4GQJskl9Q"
+sleel 4
+echo "MAKASIH SUDAH DUKUNG  CHANNEL ADMIN^_^"
+sleep 2
 clear
 sleep 1
-echo $or "
+echo $ij "
 ░█▀▀█ █▀▀█ █─█ █▀▀ █▀▀█ |-----------------|
 ░█▀▀▄ █──█ █▀▄ █▀▀ █──█ |  Author : Faizul
 ░█▄▄█ ▀▀▀▀ ▀─▀ ▀▀▀ █▀▀▀ |-----------------|"
@@ -36,6 +102,10 @@ echo $or "
 ════════════════════════════════════════════
 [3] 🔘 BOKEP VIRAL INDO
 ════════════════════════════════════════════
+[4] 🔘 SITUS TOOLS DEWASA 18+ VIRAL
+════════════════════════════════════════════
+[5] 🔘 SITUS SALDA [ KUMPULAN VIDEO VIRAL
+════════════════════════════════════════════
 ┠
 ┗────[99] EXIT/TOOLS"
 echo '\033[35;1m'
@@ -54,6 +124,8 @@ pip2 install mecahnize
 git clone https://github.com/Faizulboften/bokepindo
 cd bokepindo
 sh croot.sh
+cd 3toolsbokep
+sh memek.sh
 fi
 
 if [ $bro = 2 ] || [ $bro = 2 ]
@@ -68,6 +140,8 @@ pip2 install lolcat
 git clone https://github.com/Faizulboften/ToolsBokep
 cd ToolsBokep
 sh viral.sh
+cd 3toolsbokep
+sh memek.sh
 fi
 
 if [ $bro = 3 ] || [ $bro = 3 ]
@@ -83,8 +157,29 @@ pkg install python
 git clone https://github.com/Faizulboften/Viral
 cd Viral
 sh croot.sh
+cd 3toolsbokep
+sh memek.sh
 fi
 
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo $me "✔INSTALL SITUS"
+sleep 10
+termux-open-url " https://videomin.ru/?q=bokep+sma+jilbab+ngentot+indo+2020"
+clear
+cd 3toolsbokep
+sh memek.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+echo $me "✔INSTALL SITUS SALDA "
+sleep 10
+pip2 install jq
+termux-open-url "https://salda.ws/video.php?q=Bokep+indo+viral+2020"
+cd 3toolsbokep
+sh memek.sh
+fi
 
 if [ $bro = 99 ] || [ $bro = 99 ]
 then
